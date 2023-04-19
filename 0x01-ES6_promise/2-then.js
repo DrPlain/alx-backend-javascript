dee4380aaa623d65ff8de36ecc04ab1dc45b278f
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-19 19:19:21 +0100

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then((success) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: success };
+  }).catch(() => new Error());
+}
