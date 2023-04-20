9e49ae2b0787382325744b5b6b9d637865a6e6b6
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-20 01:15:09 +0100

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => {
+    .then((res) => (
       res.map((item) => ({
         status: item.status,
         value: item.status === 'fulfilled' ? item.value : String(item.reason),
-      }));
-    });
+      }))
+    ));
 }
