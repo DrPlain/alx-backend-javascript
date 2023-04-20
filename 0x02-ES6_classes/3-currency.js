0001051ab6bc2cbaffd119c9b64d242979f39d28
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-20 13:25:05 +0100

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Code must a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
