f7845aba1f00d19cc86c018d632264755b36549a
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-25 10:03:02 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,13 @@
 export default function cleanSet(set, startString) {
-  const string = [];
-  if (startString === '') {
+  if (!set || !startString || !(set instanceof Set) || (typeof startString !== 'string')) {
     return '';
   }
+  const string = [];
   set.forEach((item) => {
     if (item.startsWith(startString)) {
-      const remainingString = item.slice(startString.length);
+      const remainingString = item.slice(3);
       string.push(remainingString);
     }
   });
-
   return string.join('-');
 }
