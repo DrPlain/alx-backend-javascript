facac034baa69a4b1239497d36561398c1339b1a
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-28 10:18:40 +0100

    Task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,71 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getToWork(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getToWork(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+    
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting a work';
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director()
+}
+
+function isDirector(employee: Director | Teacher) {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectorTasks();
+    } else {
+        return (employee as Teacher).workTeacherTasks();
+    }
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else {
+        return 'Teaching History';
+    }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
