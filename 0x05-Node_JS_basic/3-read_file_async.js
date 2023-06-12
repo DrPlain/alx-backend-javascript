0f8baae2e6eb4d07f491a5e2a2b88a1ba2146f25
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-12 15:49:50 +0100

    Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,31 +6,33 @@ function countStudents(path) {
       if (err) {
         reject(new Error('Cannot load the database'));
       }
-      const obj = {};
-      let totalStudents = 0;
-      let lineNo = 0;
-      for (const line of data.trim().split('\n')) {
-        if (lineNo !== 0) {
-          const tokens = line.split(',');
-          if (tokens[3] in obj) {
-            totalStudents += 1;
-            obj[tokens[3]].counts += 1;
-            obj[tokens[3]].students.push(tokens[0]);
-          } else {
-            obj[tokens[3]] = {};
-            obj[tokens[3]].counts = 1;
-            obj[tokens[3]].students = [tokens[0]];
-            totalStudents += 1;
+      if (data) {
+        const obj = {};
+        let totalStudents = 0;
+        let lineNo = 0;
+        for (const line of data.trim().split('\n')) {
+          if (lineNo !== 0) {
+            const tokens = line.split(',');
+            if (tokens[3] in obj) {
+              totalStudents += 1;
+              obj[tokens[3]].counts += 1;
+              obj[tokens[3]].students.push(tokens[0]);
+            } else {
+              obj[tokens[3]] = {};
+              obj[tokens[3]].counts = 1;
+              obj[tokens[3]].students = [tokens[0]];
+              totalStudents += 1;
+            }
           }
+          lineNo += 1;
         }
-        lineNo += 1;
+        const result = [`Number of students: ${totalStudents}`];
+        for (const key of Object.keys(obj)) {
+          result.push(`Number of students in ${key}: ${obj[key].counts}. List: ${obj[key].students.join(', ')}`);
+        }
+        console.log(result.join('\n'));
+        resolve(true);
       }
-      const result = [`Number of students: ${totalStudents}`];
-      for (const key of Object.keys(obj)) {
-        result.push(`Number of students in ${key}: ${obj[key].counts}. List: ${obj[key].students.join(', ')}`);
-      }
-      console.log(result.join('\n'));
-      resolve(true);
     });
   });
 }
