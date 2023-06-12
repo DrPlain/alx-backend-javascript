85816a7546b16d86a28c182d5b7eeb7bb03ad9a6
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-12 12:06:34 +0100

    Refactored code for task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,35 @@
 const { readFileSync } = require('fs');
 
 function countStudents(path) {
-  if (!path) {
-    throw new Error('Cannot load the database');
-  }
+  let data;
+
   try {
-    const data = readFileSync(path, 'utf-8');
-    const obj = {};
-    let totalStudents = 0;
-    let lineNo = 0;
-    for (const line of data.split('\n')) {
-      if (lineNo !== 0) {
-        const tokens = line.split(',');
-        if (tokens[3] in obj) {
-          totalStudents += 1;
-          obj[tokens[3]].counts += 1;
-          obj[tokens[3]].students.push(tokens[0]);
-        } else {
-          obj[tokens[3]] = {};
-          obj[tokens[3]].counts = 1;
-          obj[tokens[3]].students = [tokens[0]];
-          totalStudents += 1;
-        }
-      }
-      lineNo += 1;
-    }
-    console.log(`Number of students: ${totalStudents}`);
-    for (const key of Object.keys(obj)) {
-      console.log(`Number of students in ${key}: ${obj[key].counts}. List: ${obj[key].students.join(', ')}`);
-    }
+    data = readFileSync(path, 'utf8');
   } catch (err) {
     throw new Error('Cannot load the database');
+  }
+  const obj = {};
+  let totalStudents = 0;
+  let lineNo = 0;
+  for (const line of data.split('\n')) {
+    if (lineNo !== 0) {
+      const tokens = line.split(',');
+      if (tokens[3] in obj) {
+        totalStudents += 1;
+        obj[tokens[3]].counts += 1;
+        obj[tokens[3]].students.push(tokens[0]);
+      } else {
+        obj[tokens[3]] = {};
+        obj[tokens[3]].counts = 1;
+        obj[tokens[3]].students = [tokens[0]];
+        totalStudents += 1;
+      }
+    }
+    lineNo += 1;
+  }
+  console.log(`Number of students: ${totalStudents}`);
+  for (const key of Object.keys(obj)) {
+    console.log(`Number of students in ${key}: ${obj[key].counts}. List: ${obj[key].students.join(', ')}`);
   }
 }
 
