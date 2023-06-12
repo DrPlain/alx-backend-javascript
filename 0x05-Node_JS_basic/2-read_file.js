e057976696da07e8e62f73af7df8faaa5401801f
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-12 11:56:22 +0100

    Refactored code for task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,28 +7,28 @@ function countStudents(path) {
   try {
     const data = readFileSync(path, 'utf-8');
     const obj = {};
-    obj.totalStudents = 0;
+    let totalStudents = 0;
     let lineNo = 0;
     for (const line of data.split('\n')) {
       if (lineNo !== 0) {
         const tokens = line.split(',');
         if (tokens[3] in obj) {
-          const fieldStudents = `${tokens[3]}Students`;
-          obj[tokens[3]] += 1;
-          obj.totalStudents += 1;
-          obj[fieldStudents].push(tokens[0]);
+          totalStudents += 1;
+          obj[tokens[3]].counts += 1;
+          obj[tokens[3]].students.push(tokens[0]);
         } else {
-          const fieldStudents = `${tokens[3]}Students`;
-          obj[tokens[3]] = 1;
-          obj[fieldStudents] = [tokens[0]];
-          obj.totalStudents += 1;
+          obj[tokens[3]] = {};
+          obj[tokens[3]].counts = 1;
+          obj[tokens[3]].students = [tokens[0]];
+          totalStudents += 1;
         }
       }
       lineNo += 1;
     }
-    console.log(`Number of students: ${obj.totalStudents}`);
-    console.log(`Number of students in CS: ${obj.CS}. List: ${obj.CSStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${obj.SWE}. List: ${obj.SWEStudents.join(', ')}`);
+    console.log(`Number of students: ${totalStudents}`);
+    for (const key of Object.keys(obj)) {
+      console.log(`Number of students in ${key}: ${obj[key].counts}. List: ${obj[key].students.join(', ')}`);
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
