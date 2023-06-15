c297e622d57cdeb2bd1d198d24d8f7f99b0ea82d
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-15 09:46:56 +0100

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -23,4 +23,11 @@ describe('calculateNumber', function() {
         result = calculateNumber(-1.5, -3.7);
         assert.strictEqual(result, -5);
     });
+    it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+        assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+      });
+
+    it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(3.6, 2.0), 6);
+    });
 })
